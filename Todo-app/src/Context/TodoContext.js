a04679dc47df270df645aacb28c4bbad927773cb
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-03-05 14:11:42 +0530

    Todo App By Hitesh sir, Context and Local Storage Learned

--- Todo-app/src/Context/TodoContext.js
+++ Todo-app/src/Context/TodoContext.js
@@ -1,8 +1,20 @@
-import { createContext, useContext } from 'react' 
+import { createContext, useContext } from "react";
 
-export const TodoContext = createContext({})
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "todo mess",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deteteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
 export const useTodo = () => {
-    return useContext(TodoContext)
-}
+  return useContext(TodoContext);
+};
 
-export const Todoprovider = TodoContext.Provider
+export const TodoProvider = TodoContext.Provider;

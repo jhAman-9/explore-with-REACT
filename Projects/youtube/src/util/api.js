77e0348f37244cc8a41bf01cbb795881f1beabb8
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-06-14 10:39:29 +0530

    Fixed Problems

--- Projects/youtube/src/util/api.js
+++ Projects/youtube/src/util/api.js
@@ -1,30 +1,3 @@
-// import axios from 'axios';
-
-// const BASE_URL = 'https://youtube138.p.rapidapi.com';
-// // const BASE_URL = 'https://youtube.googleapis.com/youtube/v3/'
-
-// const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY
-// console.log(apiKey);
-
-// const options = {
-//   params: {
-//     hl: "en",
-//     gl: "US",
-//   },
-//   headers: {
-//     "X-RapidAPI-Key": apiKey,
-//     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
-//   },
-// };
-
-
-// export const fetchDataFromApi = async (url) => {
-//   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-//   // console.log(data);
-//   return data;
-// };
-
-
 import axios from "axios";
 
 const BASE_URL = "https://youtube138.p.rapidapi.com";
@@ -41,4 +14,3 @@ export const fetchDataFromApi = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
   return data;
 };
-
